e0af184f61b95c8fc76004076af6b2b0fb8fe115
Wildanzulfikar <wildanzulfikar811@gmail.com>
2024-09-10 11:12:32 +0700

    add section

---
+++ dist/js/Detail.js
@@ -0,0 +1,10 @@
+const dataAbout = () => {
+    return [
+        {
+            id : 1,
+            title: "Computer Student Club (CSC) adalah kelompok studi mahasiswa di Politeknik Negeri Jakarta yang berfokus pada ranah keamanan siber, pengembangan perangkat lunak, dan IoT. Kami bertujuan untuk mengembangkan minat dan bakat mahasiswa serta meningkatkan prestasi mereka dalam bidang teknologi.",
+            seniman: "Menjadi wadah pengembangan minat dan bakat mahasiswa di bidang teknologi yang berfokus pada keamanan jaringan, pengembangan perangkat lunak, dan Internet of Things (IoT) yang menginspirasi inovasi dan menghasilkan pemimpin masa depan dalam dunia teknologi.",
+            deskripsi: "Memberikan pemahaman teknologi, mendukung keahlian mahasiswa, menyediakan platform eksplorasi, mendorong kolaborasi, menginspirasi inovasi, mendukung prestasi, dan mempromosikan etika serta keamanan teknologi."
+        }
+    ]
+}
