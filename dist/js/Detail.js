419ea6e031ffef1052c5eba807e2ab0f53ad2fc5
Wildanzulfikar <wildanzulfikar811@gmail.com>
2024-09-11 23:08:35 +0700

    final

--- dist/js/Detail.js
+++ dist/js/Detail.js
@@ -85,3 +85,11 @@ navigation: {
     prevEl: ".swiper-button-prev",
 },
 });
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const hamburgerMenuIcon  = document.getElementById('hamburger-menu i');
+const drowpdownMenu = document.getElementById('dropdown-menu');
+
+hamburgerMenu.onclick = function() {
+    drowpdownMenu.classList.toggle('open');
+}
