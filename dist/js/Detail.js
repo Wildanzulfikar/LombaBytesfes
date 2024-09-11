8fb270e824302a37f6aa2a5f160986802546bc64
Wildanzulfikar <wildanzulfikar811@gmail.com>
2024-09-11 13:43:35 +0700

    add file

--- dist/js/Detail.js
+++ dist/js/Detail.js
@@ -1,10 +1,88 @@
-const dataAbout = () => {
-    return [
-        {
-            id : 1,
-            title: "Computer Student Club (CSC) adalah kelompok studi mahasiswa di Politeknik Negeri Jakarta yang berfokus pada ranah keamanan siber, pengembangan perangkat lunak, dan IoT. Kami bertujuan untuk mengembangkan minat dan bakat mahasiswa serta meningkatkan prestasi mereka dalam bidang teknologi.",
-            seniman: "Menjadi wadah pengembangan minat dan bakat mahasiswa di bidang teknologi yang berfokus pada keamanan jaringan, pengembangan perangkat lunak, dan Internet of Things (IoT) yang menginspirasi inovasi dan menghasilkan pemimpin masa depan dalam dunia teknologi.",
-            deskripsi: "Memberikan pemahaman teknologi, mendukung keahlian mahasiswa, menyediakan platform eksplorasi, mendorong kolaborasi, menginspirasi inovasi, mendukung prestasi, dan mempromosikan etika serta keamanan teknologi."
-        }
-    ]
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+  }
+  
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
 }
+  
+function checkWidth() {
+    const box = document.getElementById('hamburger-menu');
+    
+    if (window.matchMedia("(min-width: 1022px)").matches) {
+        box.classList.add('hidden');
+    } else {
+        box.classList.remove('hidden');
+    }
+}
+
+checkWidth();
+
+window.addEventListener('resize', checkWidth);
+
+
+const swiper = new Swiper(".mySwiper", {
+pagination: {
+  el: ".swiper-pagination",
+  dynamicBullets: true,
+},
+});
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+const swipers = new Swiper(".mySwipers", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+},
+
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true
+},
+
+navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev"
+},
+
+on: {
+  autoplayTimeLeft(s, time, progress) {
+    progressCircle.style.setProperty("--progress", 1 - progress);
+    progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+  }
+}
+});
+
+const swipersx = new Swiper(".mySwipersx", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+},
+pagination: {
+    el: ".swiper-pagination",
+},
+});
+
+var swiperx = new Swiper(".mySwiperx", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+});
